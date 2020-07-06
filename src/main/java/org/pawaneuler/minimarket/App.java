947e107e6c4dcb1c48338f1e659d33ef7b9cf7d4
Fauzan Lubis <fauzanfauh45@gmail.com>
2020-07-06 15:57:59 +0700

    Initialization of Java project

---
+++ src/main/java/org/pawaneuler/minimarket/App.java
@@ -0,0 +1,13 @@
+package org.pawaneuler.minimarket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

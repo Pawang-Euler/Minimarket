a2f5ba801fe075157fb8379f098463ecd5c98af8
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-07-18 08:04:56 +0700

    do stress test for TransactionLogGenerator

---
+++ src/test/java/org/pawaneuler/Experimentation/TransactionLogGenerator/StressTest/StressTest.java
@@ -0,0 +1,44 @@
+package org.pawaneuler.Experimentation.TransactionLogGenerator.StressTest;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.pawaneuler.Generator.TransactionLogGenerator.TransactionLogGenerator;
+import org.pawaneuler.IOTools.Exceptions.BadExtentionException;
+
+public class StressTest {
+    private int maxVariety = 26;
+    private int numOfTransaction = 10000000;
+    private TransactionLogGenerator generator;
+
+    @Test
+    public void mainTest() {
+        if (maxVariety > 26)
+            maxVariety = 26;
+
+        this.generator = new TransactionLogGenerator(maxVariety);
+
+        this.doStressTest();
+    }
+
+    private void doStressTest() {
+        String filePath = this.createFilePath();
+
+        try {
+            this.generator.generate(numOfTransaction, filePath);
+        } catch (BadExtentionException e) {
+            System.out.println(e);
+            fail();
+        } catch (IOException e) {
+            System.out.println(e);
+            fail();
+        }
+    }
+
+    private String createFilePath() {
+        return "src/test/java/org/pawaneuler/Experimentation/TransactionLogGenerator/StressTest/TransactionLogs/" + maxVariety + "-" + numOfTransaction + ".tcsv";
+    }
+}

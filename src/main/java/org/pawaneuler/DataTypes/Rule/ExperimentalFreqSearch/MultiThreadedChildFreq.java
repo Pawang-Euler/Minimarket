3f4cfadbaf5b5fee646e0336678edd5e8d7b8771
Fauzan Lubis <fauzanfauh45@gmail.com>
2020-07-19 13:35:45 +0700

    Using normal recursion on low number of child

--- src/main/java/org/pawaneuler/DataTypes/Rule/ExperimentalFreqSearch/MultiThreadedChildFreq.java
+++ src/main/java/org/pawaneuler/DataTypes/Rule/ExperimentalFreqSearch/MultiThreadedChildFreq.java
@@ -9,10 +9,38 @@ import org.pawaneuler.DataTypes.Trie.Trie;
 public class MultiThreadedChildFreq extends RecursiveTask<Integer> {
     private Trie trie;
     private int currentNodeIndex;
+    private final int CHILD_THRESHOLD = 3;
 
     public MultiThreadedChildFreq(Trie trie, int currentNodeIndex) {
         this.trie = trie;
         this.currentNodeIndex = currentNodeIndex;
+    }
+
+    private int normalRecursionAtNodeIndex(int nodeIndex) {
+        Node currNode = trie.getNodeAt(nodeIndex);
+
+        if (this.isNodeEligibleForSplitting(currNode)) {
+            return this.splitRecursionStartingOnNodeIndexed(nodeIndex);
+        } else {
+            return this.continueRecursionOnAllChildOfNodeIndexed(nodeIndex);
+        }
+    }
+
+    private int continueRecursionOnAllChildOfNodeIndexed(int nodeIndex) {
+        ArrayList<Integer> childIndexes = this.getAllChildIndexOfNodeIndexed(nodeIndex);
+        int totalChildFrequency = 0;
+
+        for (Integer childIndex : childIndexes) {
+            totalChildFrequency += this.normalRecursionAtNodeIndex(childIndex);
+        }
+
+        return totalChildFrequency;
+    }
+
+    private int splitRecursionStartingOnNodeIndexed(int nodeIndex) {
+        this.currentNodeIndex = nodeIndex;
+
+        return this.splitRecursion();
     }
 
     private int splitRecursion() {
@@ -45,15 +73,23 @@ public class MultiThreadedChildFreq extends RecursiveTask<Integer> {
         return new MultiThreadedChildFreq(this.trie, nodeIndex);
     }
 
-    private ArrayList<Integer> getAllChildIndex() {
+    private ArrayList<Integer> getAllChildIndexOfNodeIndexed(int nodeIndex) {
         ArrayList<Integer> childIndex = new ArrayList<Integer>();
-        Node currentNode = trie.getNodeAt(this.currentNodeIndex);
+        Node currentNode = trie.getNodeAt(nodeIndex);
 
         for (int i = 0; i < currentNode.getDegree(); i++) {
             childIndex.add(currentNode.getChildIndexAt(i));
         }
 
         return childIndex;
+    }
+
+    private ArrayList<Integer> getAllChildIndex() {
+        return this.getAllChildIndexOfNodeIndexed(this.currentNodeIndex);
+    }
+
+    private boolean isNodeEligibleForSplitting(Node node) {
+        return node.getDegree() >= CHILD_THRESHOLD;
     }
 
     @Override
